3ec8acab02fc21b2f1f979d718228fe5888c78ac
emonmahdi <developeremon1997@gmail.com>
2023-05-30 08:52:07 +0600

    testing

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,16 @@
-import express, { Application, Request, Response } from "express";
-import cors from 'cors';
-const app: Application = express();
-const port = 3000;
-
-
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
 
 // middleware
-app.use(cors());
-
+                    app.use(cors())
 
 //parser
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
+app.get('/', (req: Request, res: any) => {
+  res.send('University management database successfully')
+})
 
-
-app.get("/", (req:Request, res:Response) => {
-  res.send("University management database successfully");
-});
-
-export default app;
+export default app
--- src/server.ts
+++ src/server.ts
@@ -3,19 +3,16 @@ import config from './config/index'
 import app from './app'
 
 async function bootstrap() {
-    try{
-        await mongoose.connect(config.database_url as string);
-        console.log("Database connection successfully");
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log('Database connection successfully')
 
-        app.listen(config.port, () => {
-            console.log(`Application app listening on port ${config.port}`)
-          })
-
-    }catch(error){
-        console.log('Failed to connect')
-    }
-    
-   
+    app.listen(config.port, () => {
+      console.log(`Application app listening on port ${config.port}`)
+    })
+  } catch (error) {
+    console.log('Failed to connect')
+  }
 }
 
-bootstrap();
+bootstrap()
