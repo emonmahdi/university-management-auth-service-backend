4e2dd080db162494bdbb3a66f993fac053833195
emonmahdi <developeremon1997@gmail.com>
2023-06-02 06:50:20 +0600

    imporve lintin

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import usersRouter from './app/modules/users/users.route'
 const app: Application = express()
+
+// import usersService from './app/modules/users/users.service'
 
 // middleware
 app.use(cors())
@@ -9,7 +12,10 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: Response) => {
+// Application data
+app.use('/api/v1/users/', usersRouter)
+
+app.get('/', async (req: Request, res: Response) => {
   res.send('University management database successfully')
 })
 
